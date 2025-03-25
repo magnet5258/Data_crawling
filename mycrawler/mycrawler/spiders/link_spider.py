ae85451cef25eb3c225ad98f69a191d47c90038b
양동환 <magnet0922@naver.com>
2025-03-25 12:51:02 +0900

    modify spiders

--- mycrawler/mycrawler/spiders/link_spider.py
+++ mycrawler/mycrawler/spiders/link_spider.py
@@ -1,3 +1,5 @@
+import scrapy
+
 class LinkSpider(scrapy.Spider):
     name = "link_spider"
     start_urls = ["http://quotes.toscrape.com/"]
@@ -5,24 +7,32 @@ class LinkSpider(scrapy.Spider):
     def __init__(self):
         self.visited_urls = set()
         self.url_count = 0
-        self.max_urls = 1000  # 수집 제한
+        self.max_urls = 1000
 
     def parse(self, response):
+        # 중복 URL 제거
         if response.url in self.visited_urls:
             return
         self.visited_urls.add(response.url)
 
+        # 수집 수 제한
         if self.url_count >= self.max_urls:
             return
         self.url_count += 1
 
+        # 현재 depth 추적
         current_depth = response.meta.get('depth', 0)
+
+        # 현재 페이지 정보 저장
         yield {
             'depth': current_depth,
             'url': response.url,
+            'referer': response.request.headers.get('Referer', None).decode() \
+                if response.request.headers.get('Referer') else None
         }
 
-        if current_depth < 3:
+        # 다음 페이지 링크 탐색 (depth 제한 + 내부 링크만)
+        if current_depth < 4:
             urls = response.css("a::attr(href)").getall()
             for url in urls:
                 if url.startswith("/"):  # 내부 링크만
--- mycrawler/mycrawler/spiders/quote_spider.py
+++ mycrawler/mycrawler/spiders/quote_spider.py
@@ -1,21 +1,27 @@
+import scrapy
+
 class QuoteSpider(scrapy.Spider):
-    name = "quotes"
+    name = "quote_spider"
     start_urls = ['https://quotes.toscrape.com/']
 
+    def __init__(self):
+        self.seen_quotes = set()
+
     def parse(self, response):
-        # 현재 페이지 번호 추적
         page_num = response.meta.get('page_num', 1)
 
         quotes = response.css('div.quote')
         for quote in quotes:
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall()
-            }
+            text = quote.css('span.text::text').get()
+            if text not in self.seen_quotes:
+                self.seen_quotes.add(text)
+                yield {
+                    'text': text,
+                    'author': quote.css('small.author::text').get(),
+                    'tags': quote.css('div.tags a.tag::text').getall()
+                }
 
-        # 다음 페이지가 있고, 페이지 수 제한 안 넘으면 계속 탐색
-        if page_num < 3:
+        if page_num < 20:
             next_page = response.css('li.next a::attr(href)').get()
             if next_page:
                 yield response.follow(
